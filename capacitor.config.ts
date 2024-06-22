ab1974eb77a330dcd43cb187782eff59fb6da903
Pranav C Kulkarni <pranav.kulkarni@ecolab.com>
2024-06-22 17:21:56 +0530

    Update - new commit changes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Elite_Schedule_App',
+  appName: 'Elite_Schedule_App',
+  webDir: 'www'
+};
+
+export default config;
